5e761b242b2bd4f17c725e7c2ee689a702e4df0a
Serge-In <aa11@mail.ru>
2023-03-21 12:10:28 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        int i = 1;
+
+        for (int j = 1; j < 10; j++){
+            System.out.println(i + " * " + j + " = " + i * j);
+        }
+    }
+}
