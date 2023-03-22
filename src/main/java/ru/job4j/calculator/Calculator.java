f274c592b10a7804a3d7f024f7425e0043e15878
Serge-In <aa11@mail.ru>
2023-03-22 15:17:17 +0300

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,23 +1,21 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static int func1(int x) {
+        return  x * x + 1;
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
+        Calculator.plus(1, 2);
+        Calculator.plus(10, 11);
 
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        int size = 10;
-
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
-
+        int result3 = func1(100);
+        System.out.println(result3);
     }
 }
