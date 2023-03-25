8310ee48d92deca4f338c99e281bf10a5466b47d
Serge-In <aa11@mail.ru>
2023-03-25 11:27:36 +0300

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -5,13 +5,9 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
             rsl = "Hi, SmartAss.";
-        } else if ("Bye.".equals(question)) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+        } else if ("Bye.".equals(question)) {
             rsl = "See you later.";
-        } else {
-            rsl = "I don't know. Please, ask another question.";
         }
         return rsl;
     }
@@ -20,6 +16,8 @@ public class DummyBot {
         String rsl = DummyBot.answer("Hi, Bot.");
         System.out.println(rsl);
         rsl = DummyBot.answer("Bye.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("anyelse");
         System.out.println(rsl);
     }
 }
