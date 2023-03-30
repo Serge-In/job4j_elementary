0c4d0ca745200d7df31ed7adaa415341f901918e
Serge-In <aa11@mail.ru>
2023-03-30 13:11:53 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,8 @@ public class Mortgage {
     public static int year(double loan, int salary, double percent) {
         int year = 0;
         while (loan > 0) {
-            loan = loan + loan * percent - salary;
+            //loan = loan + loan * percent - salary;
+            loan += loan * percent - salary;
             year++;
         }
         return year;
