71f2b26aaade2f966e3ece4cb27e7cec29063f1d
Serge-In <aa11@mail.ru>
2023-03-31 13:07:07 +0300

    6.2.3. Перевернуть массив. [#226790]

---
+++ src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        if (array.length == 1) {
+            return array;
+        }
+        for (int i = 0; i <= array.length / 2 - 1; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        return array;
+    }
+}
---
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TurnTest {
+    @Test
+    void backIn0Out0() {
+        int[] in = {0};
+        int[] expected = {0};
+        int[] result = Turn.back(in);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void backIn01Out10() {
+        int[] in = {0, 1};
+        int[] expected = {1, 0};
+        int[] result = Turn.back(in);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void backIn012Out210() {
+        int[] in = {0, 1, 2};
+        int[] expected = {2, 1, 0};
+        int[] result = Turn.back(in);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void backIn0123456Out6543210() {
+        int[] in = {0, 1, 2, 3, 4, 5, 6};
+        int[] expected = {6, 5, 4, 3, 2, 1, 0};
+        int[] result = Turn.back(in);
+        assertThat(result).containsExactly(expected);
+    }
+}
