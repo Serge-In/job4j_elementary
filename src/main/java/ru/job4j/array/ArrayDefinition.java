38773e51d32ef6ac2c5f445ecaf870d0b2dfd2e5
Serge-In <aa11@mail.ru>
2023-04-01 10:23:03 +0300

    6.0.2. Размер массива [#333582] refactor

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,7 +6,7 @@ public class ArrayDefinition {
         System.out.println("ages.length = " + ages.length);
         String[] surnames = new String[100500];
         System.out.println("surnames.length = " + surnames.length);
-        float[] prices = new float[40];
+        float[] prices = new float[50];
         System.out.println("prices.length = " + prices.length);
         String[] names = new String[4];
         names[0] = "Surya";
