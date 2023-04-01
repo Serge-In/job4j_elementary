2e64e81ede5537399e8a8617cbb5979b788e651b
Serge-In <aa11@mail.ru>
2023-04-01 13:39:40 +0300

    6.7.1. Моно строка в матрице. [#214126]

---
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+/**
+ * 6.7.1. Моно строка в матрице. [#214126]
+ * Проверяет заполнение строки массива символов
+ * возвращает true, если в заданной строке массива содержатся только пробел или 'X', иначе - false
+ * 2023-04-01
+ */
+public class MatrixCheck {
+    public static boolean monoHorizontal(char[][] board, int row) {
+        for (int j = 0; j < board[row].length; j++) {
+            if (!(board[row][j] == 'X' || board[row][j] == ' ')) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
