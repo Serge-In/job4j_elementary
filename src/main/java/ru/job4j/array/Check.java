3214f03efafd4df0ebd101aa14ed34bf6f788093
Serge-In <aa11@mail.ru>
2023-04-01 10:31:22 +0300

    6.3. Массив заполнен true или false [#53857] refactor

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -1,9 +1,13 @@
 package ru.job4j.array;
 
+/**
+ * Проверяет, что элементы в массиве являются либо ВСЕ true либо ВСЕ false
+ * Решение задачи сводится к проверке всех элементов с эталонным (с первым элементом массива)
+ */
 public class Check {
     public static boolean mono(boolean[] data) {
-        for (boolean element : data) {
-            if (element != data[0]) {
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] != data[0]) {
                 return false;
             }
         }
