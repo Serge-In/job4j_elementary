6ece83c602131b723cd24706b4a8d1bb896b39bb
Serge-In <aa11@mail.ru>
2023-04-01 16:11:47 +0300

    6.7.2. Моно столбец в матрице. [#214127]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -15,4 +15,19 @@ public class MatrixCheck {
         }
         return true;
     }
+
+    /**
+     * 6.7.2. Моно столбец в матрице. [#214127]
+     * Проверяет заполнение колонки массива символов
+     * возвращает true, если в заданной колонке массива содержатся только 'X', иначе - false
+     * 2023-04-01
+     */
+    public static boolean monoVertical(char[][] board, int column) {
+        for (char[] chars : board) {
+            if (chars[column] != 'X') {
+                return false;
+            }
+        }
+        return true;
+    }
 }
