085392604a2fad3b4c86b8dacea7278690ebd78d
Serge-In <geotod@mail.ru>
2023-06-07 22:43:01 +0300

    refactor

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        char symbol = input.charAt(0);
+        int counter = 1;
+        String result = "";
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == symbol) {
+                counter++;
+            } else if (counter == 1) {
+                result += String.valueOf(symbol);
+                symbol = input.charAt(i);
+            } else {
+                result += String.valueOf(symbol) + counter;
+                symbol = input.charAt(i);
+                counter = 1;
+            }
+        }
+        if (counter == 1) {
+            result += String.valueOf(symbol);
+        } else {
+            result += String.valueOf(symbol) + counter;
+        }
+        return result;
+    }
+}
